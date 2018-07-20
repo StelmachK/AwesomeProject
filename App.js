abf2276a832f42bc2603fd3f1494783faaa94850
unknown <kacper.stelmach@fenergo.com>
2018-07-20 13:45:17 +0200

    states

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet } from 'react-native';
 
-export default class App extends React.Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 10000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : 'chuja';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class BlinkApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>: DDD </Text>
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
 }
+
+// skip this line if using Create React Native App
+//AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
 
 const styles = StyleSheet.create({
   container: {
